0425a9bf1251d961df27ea1c5f7986c68eff38fd
Pongsametrey Sok <metreysk@gmail.com>
2021-09-04 13:12:37 +0700

    upgrade to Androidx

--- app/src/main/java/com/example/admin/androiddatabaseexample/DisplayInfoActivity.java
+++ app/src/main/java/com/example/admin/androiddatabaseexample/DisplayInfoActivity.java
@@ -1,6 +1,6 @@
 package com.example.admin.androiddatabaseexample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DisplayInfoActivity extends AppCompatActivity {
--- app/src/main/java/com/example/admin/androiddatabaseexample/SignUp.java
+++ app/src/main/java/com/example/admin/androiddatabaseexample/SignUp.java
@@ -1,6 +1,6 @@
 package com.example.admin.androiddatabaseexample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.Context;
